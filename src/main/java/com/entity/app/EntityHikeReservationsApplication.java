1873678662772b5c2f8df442ec9e9f6fcde95d73
Kunal Malhotra <kunal.malhotra@idemia.com>
2022-02-03 23:36:41 +0530

    Initial Project Structure

---
+++ src/main/java/com/entity/app/EntityHikeReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.entity.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityHikeReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityHikeReservationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entity/app/EntityHikeReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entity.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityHikeReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
